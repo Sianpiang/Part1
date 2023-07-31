52de57c99963db6f6ae6ba8304b599df225e5dbf
Sianpiang <sianpiang8@gmail.com>
2023-07-31 16:54:40 +0530

    Complete 1.6&1.7

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+const Header = ({heading})=>{
+  return(
+    <h1>{heading}</h1>
+  )
+}
+const Button = ({handleClick,reviews})=>{
+  return(
+    <button onClick={()=>handleClick(reviews)}>{reviews}</button> 
+  )
+}
+const Display = ({good,neutral,bad})=>{
+  const total = good+neutral+bad;
+  const avg = total/3;
+  const posi = (good*100)/total
+  return(
+    <>
+      <p>{`Good: ${good} Neutral: ${neutral} Bad: ${bad}`}</p> 
+      <p>Total:{total}</p> 
+      <p>Average:{avg}</p> 
+      <p>Positive %:{posi}</p> 
+    </>
+  )
+}
 
 function App() {
+  const [good,setGood]=useState(0);
+  const [neutral,setNeutral]=useState(0);
+  const [bad,setBad]=useState(0);
+
+  const handleClick = (btns,e)=>{
+    if(btns==="Good"){
+      setGood(
+        prev=>prev+1
+      )
+    }
+    else if(btns==="Neutral"){
+      setNeutral(
+        prev=>prev+1
+      )
+    }
+    else{
+      setBad(
+        prev=>prev+1
+      )
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header
+        heading="Give feedback"
+      />
+      <Button
+        handleClick={handleClick}
+        reviews="Good"
+      />
+      <Button
+        handleClick={handleClick}
+        reviews="Neutral"
+      />
+      <Button
+        handleClick={handleClick}
+        reviews="Bad"
+      />
+      <Display
+        good={good}
+        neutral={neutral}
+        bad={bad}
+      />
     </div>
   );
 }
