9dc7dd5fc9bd7b3564af163460bc1e7d907c61a4
Sianpiang <sianpiang8@gmail.com>
2023-07-31 18:03:47 +0530

    Complete unicafe

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -10,16 +10,68 @@ const Button = ({handleClick,reviews})=>{
     <button onClick={()=>handleClick(reviews)}>{reviews}</button> 
   )
 }
-const Display = ({good,neutral,bad})=>{
+const Statistic = ({good,neutral,bad,title})=>{
   const total = good+neutral+bad;
-  const avg = total/3;
-  const posi = (good*100)/total
+  const avg = (good-bad)/total;
+  const posi = Math.floor((good*100)/total);
   return(
     <>
-      <p>{`Good: ${good} Neutral: ${neutral} Bad: ${bad}`}</p> 
-      <p>Total:{total}</p> 
-      <p>Average:{avg}</p> 
-      <p>Positive %:{posi}</p> 
+      <h2>{title}</h2>
+      {good!==0||neutral!=0||bad!==0? 
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              Good:
+            </td>
+            <td>
+              {good}
+            </td>
+          </tr>
+          <tr>
+            <td>
+              Neutral:
+            </td>
+            <td>
+              {neutral}
+            </td>
+          </tr>
+          <tr>
+            <td>
+              Bad:
+            </td>
+            <td>
+              {bad}
+            </td>
+          </tr>
+          <tr>
+            <td>
+              Total:
+            </td>
+            <td>
+              {total}
+            </td>
+          </tr>
+          <tr>
+            <td>
+              Average:
+            </td>
+            <td>
+              {avg}
+            </td>
+          </tr>
+          <tr>
+            <td>
+              Positive%:
+            </td>
+            <td>
+              {posi}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      :  <h4>No feedback given</h4>
+      }
     </>
   )
 }
@@ -29,7 +81,7 @@ function App() {
   const [neutral,setNeutral]=useState(0);
   const [bad,setBad]=useState(0);
 
-  const handleClick = (btns,e)=>{
+  const handleClick = (btns)=>{
     if(btns==="Good"){
       setGood(
         prev=>prev+1
@@ -63,7 +115,8 @@ function App() {
         handleClick={handleClick}
         reviews="Bad"
       />
-      <Display
+      <Statistic
+        title="Statistic"
         good={good}
         neutral={neutral}
         bad={bad}
